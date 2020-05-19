8a0fd67a9e59524881ec0fb328b5c412bfcb5aa3
oalfaruque <oalfaruque@gmail.com>
2020-05-19 02:59:53 -0400

    Edited

--- src/commonjavaquestion/FindGCDOfTwoNumbersUsingRecursion.java
+++ src/commonjavaquestion/FindGCDOfTwoNumbersUsingRecursion.java
@@ -1,18 +1,14 @@
 package commonjavaquestion;
 //Write a Java program to find GCD(Greatest Common Denominator) of two numbers
 public class FindGCDOfTwoNumbersUsingRecursion {
-    public static int findingGCDofTwoNumbersUsingRecursion(int number1, int number2){
-        if (number1>number2){
-            if (number2 !=0){
-                for(int i = 1; i <=number1; i++){
-                    if (number1 % i == 0 && number2 % i == 0)
-                        return i;
-                }
-            }
-        }return findingGCDofTwoNumbersUsingRecursion(number1, number2);
+
+    public static int findingGCDusingRecursion(int number1, int number2){
+        if(number2!=0)
+                return findingGCDusingRecursion(number2, number1%number2);
+            else return  number1;
     }
 
     public static void main(String[] args) {
-        System.out.println(findingGCDofTwoNumbersUsingRecursion(36,6));
+        System.out.println("Greatest Common Denominator(GCD) is: " +findingGCDusingRecursion(60,30));
     }
 }
